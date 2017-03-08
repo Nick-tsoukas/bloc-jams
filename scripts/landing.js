1fe69311569fe8999f8816eb196841f7dcbd58de
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2017-03-08 14:42:02 -0600

    Assignment 23 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,20 +1,17 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point){
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+
+
 var animatePoints = function(points) {
- 
-    
-    
-var revealPoint = function(index){
-  points[index].style.opacity = 1;
-  points[index].style.transform = "scaleX(1) translateY(0)";
-  points[index].style.msTransform = "scaleX(1) translateY(0)";
-  points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+  forEach(points,revealPoint); 
 }
-       
-for(var i=0;i<points.length;i++){
-  revealPoint(i);
-}
-};
+
 
 window.onload = function(){
   if (window.innerHeight > 950) {
@@ -23,11 +20,12 @@ window.onload = function(){
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
   window.addEventListener('scroll', function(event){
-  console.log(event);
-      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-        animatePoints(pointsArray);  
-      }
+    console.log(event);
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);  
+    }
  
   });
 }
+
 
