fb17142351d08ceded3d6bfa88497328890362d3
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2017-03-15 22:42:30 -0500

    checkpoint-32:completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,29 @@
+ var albumPicasso = {
+    name: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { name: 'Blue', duration: '4:26' },
+        { name: 'Green', duration: '3:14' },
+        { name: 'Red', duration: '5:01' },
+        { name: 'Pink', duration: '3:21' },
+        { name: 'Magenta', duration: '2:15' }
+    ]
+};
+
+var albumMarconi = {
+    name: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { name: 'Hello, Operator?', duration: '1:01' },
+        { name: 'Ring, ring, ring', duration: '5:01' },
+        { name: 'Fits in your pocket', duration: '3:21' },
+        { name: 'Can you hear me now?', duration: '3:14' },
+        { name: 'Wrong phone number', duration: '2:15' }
+    ]
+};
